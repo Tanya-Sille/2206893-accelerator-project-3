31b23da2cddccdb21bf5e873b19a0e79e66c1ba5
tanya.sille <tanya.sille@gmail.com>
2024-09-23 03:00:38 +0300

    добавляет частичную реализацию формы

--- source/js/main.js
+++ source/js/main.js
@@ -2,8 +2,10 @@
 import './modules/slider-promo.js';
 import {openMainMenu, clickMenuLinks, clickSubMenuLinks} from './modules/menu.js';
 import {setPromoBackground} from './modules/promo-background.js';
+import {activateFormSelect} from './modules/form-select.js';
 
 openMainMenu();
 clickMenuLinks();
 clickSubMenuLinks();
 setPromoBackground();
+activateFormSelect();
---
+++ source/js/modules/form-select.js
@@ -0,0 +1,68 @@
+const select = document.querySelector('.form__select');
+const selectTitle = select.querySelector('.form__select-title');
+const selectLabels = select.querySelectorAll('.form__select-label');
+const selectOptions = select.querySelectorAll('.form__select-option');
+let selectedOption = document.querySelector('.form__select-option:checked');
+
+const onDocumentClick = (evt) => {
+  if (!evt.target.closest('.form__select')) {
+    evt.preventDefault();
+    select.setAttribute('data-state', '');
+    selectLabels.forEach((selectLabel) => selectLabel.setAttribute('tabindex', '-1'));
+  }
+};
+
+const changeSelectedOption = (index) => {
+  selectedOption.setAttribute('checked', 'false');
+  selectedOption = selectOptions[index];
+  selectedOption.setAttribute('checked', 'true');
+  selectLabels.forEach((selectLabel) => selectLabel.classList.remove('form__select-label--active'));
+  selectLabels[index].classList.add('form__select-label--active');
+};
+
+const setSelectState = () => {
+  if (select.getAttribute('data-state') === 'active') {
+    select.setAttribute('data-state', '');
+    selectLabels.forEach((selectLabel) => selectLabel.setAttribute('tabindex', '-1'));
+    document.removeEventListener('click', onDocumentClick);
+  } else {
+    select.setAttribute('data-state', 'active');
+    selectLabels.forEach((selectLabel) => selectLabel.setAttribute('tabindex', '0'));
+    select.querySelector('.form__select-label--active').focus();
+    document.addEventListener('click', onDocumentClick);
+  }
+};
+
+const onSelectTitleClick = () => selectTitle.addEventListener('click', () => setSelectState());
+
+const onSelectOptionClick = (evt, index) => {
+  selectTitle.textContent = evt.target.dataset.value;
+  changeSelectedOption(index);
+  setSelectState();
+};
+
+const onSelectOptionKeyClick = (evt, index) => {
+  if (evt.code === 'Enter') {
+    setSelectState();
+  } else if (evt.code === 'ArrowDown' && index !== selectLabels.length - 1) {
+    evt.preventDefault();
+    selectLabels[index + 1].focus();
+    changeSelectedOption(index + 1);
+    selectTitle.textContent = selectLabels[index + 1].dataset.value;
+  } else if (evt.code === 'ArrowUp' && index !== 0) {
+    evt.preventDefault();
+    selectLabels[index - 1].focus();
+    changeSelectedOption(index - 1);
+    selectTitle.textContent = selectLabels[index - 1].dataset.value;
+  }
+};
+
+const activateFormSelect = () => {
+  onSelectTitleClick();
+  selectLabels.forEach((selectLabel, index) => {
+    selectLabel.addEventListener('click', (evt) => onSelectOptionClick(evt, index));
+    selectLabel.addEventListener('keydown', (evt) => onSelectOptionKeyClick(evt, index));
+  });
+};
+
+export {activateFormSelect};
